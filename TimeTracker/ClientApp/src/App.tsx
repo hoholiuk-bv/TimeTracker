0ffe875437f97591f99e08708d531071a0637844
Oleksandr Korenivskyi <sana22_koo@student.ztu.edu.ua>
2023-06-22 15:21:34 +0300

    Initial commit

---
+++ TimeTracker/ClientApp/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Dream team React app</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ TimeTracker/ClientApp/src/AppRoutes.ts
@@ -0,0 +1,16 @@
+const AppRoutes = [
+  // {
+  //   index: true,
+  //   element: <Home />
+  // },
+  // {
+  //   path: '/counter',
+  //   element: <Counter />
+  // },
+  // {
+  //   path: '/fetch-data',
+  //   element: <FetchData />
+  // }
+];
+
+export default AppRoutes;
