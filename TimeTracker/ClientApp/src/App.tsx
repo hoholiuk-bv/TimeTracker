86351e58a91e2d043fe3e069f1e0f491a405213c
Andrii <latasandrej@gmail.com>
2023-08-21 15:23:34 +0300

    deleted unnecessary files

--- TimeTracker/ClientApp/src/App.tsx
+++ TimeTracker/ClientApp/src/App.tsx
@@ -16,7 +16,6 @@ import { UserDetailsPage } from './components/userInfo/details/UserDetailsPage';
 import { UserDaysOffPage } from './components/userInfo/daysOff/UserDaysOffPage';
 import { CalendarSettingsPage } from './components/calendarSettings';
 import { UserWorktimePage } from './components/userInfo/worktime/UserWorktimePage';
-import {PageForFullTimer} from './components/common/PageForFullTimer';
 
 const App = () => {
   return (
@@ -92,9 +91,9 @@ const App = () => {
               index
               path={routes.worktime}
               element={
-                <PageForFullTimer worktimePermission={[PermissionType.UseWorktimeTimer]}>
+                <Page>
                   <WorktimePage />
-                </PageForFullTimer>
+                </Page>
               }
             />
             <Route
--- TimeTracker/ClientApp/src/components/worktimePage/WorktimePage.tsx
+++ TimeTracker/ClientApp/src/components/worktimePage/WorktimePage.tsx
@@ -7,6 +7,8 @@ import { Button } from 'react-bootstrap';
 import { WorkCalendarModal } from '../calendar/WorkCalendarModal';
 import { WorktimeListSection } from './WorktimeListSection';
 import '../../custom.css';
+import {usePermissions} from '../../behavior/hooks';
+import {PermissionType} from '../../behavior/profile/types';
 
 export const WorktimePage = () => {
   const dispatch = useDispatch();
@@ -14,6 +16,8 @@ export const WorktimePage = () => {
   const user = useSelector((state: RootState) => state.profile.userInfo);
   const { filtering } = useSelector((state: RootState) => state.worktime);
   const [showCalendarModal, setShowCalendarModal] = useState(false);
+  const [canUseWorktimeTimer] = usePermissions([PermissionType.UseWorktimeTimer]);
+
 
   useEffect(() => {
     if (user !== null) {
@@ -31,9 +35,11 @@ export const WorktimePage = () => {
       <Button className='btn btn-primary my-3 me-3' onClick={() => setShowCalendarModal(true)}>
         View calendar
       </Button>
+      {canUseWorktimeTimer &&
         <div className='worktime-button'>
         <PartTimerPage users={user} worktime={worktime} />
         </div>
+      }
         <WorkCalendarModal show={showCalendarModal} handleClose={() => setShowCalendarModal(false)} />
       <WorktimeListSection userId={user.id}/>
     </>
